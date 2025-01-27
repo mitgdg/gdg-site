5571e6f36782da43a503ae3c0661e27ac3e5a1ea
William Y. Feng <womogenes2@gmail.com>
2025-01-27 17:48:24 -0500

    Make gim larger, make text black

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -22,37 +22,45 @@ export default function Scene() {
       setScene(gltf.scene);
 
       const fadeInDuration = 200;
-      const startTime = new Date();
 
+      let startTime;
       const animate = () => {
         const timer = new Date() - startTime;
         setGimPosition([
           0,
-          -(1 + Math.exp(-timer * 0.005) * 2) * // Fancy math function for bounce in
+          -(1 + Math.exp(-timer * 0.002) * 2) * // Fancy math function for bounce in
             Math.cos(-timer * 0.003) * // Bounce period
-            0.2,
+            0.2 -
+            0.5,
           0,
         ]);
-        if (timer < fadeInDuration) setGimOpacity(timer / fadeInDuration);
+        if (timer <= fadeInDuration) setGimOpacity(timer / fadeInDuration);
+        else setGimOpacity(100);
         requestAnimationFrame(animate);
       };
-      animate();
+      setTimeout(() => {
+        startTime = new Date();
+        animate();
+      }, 50);
     });
   }, []);
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center px-8">
       <div className="relative cursor-default">
-        <h1 className="z-20 text-center text-8xl font-black tracking-tighter text-purple-900">
-          MIT GAME DEVELOPERS GUILD
+        <h1 className="z-20 mb-10 mr-36 max-w-md text-center text-8xl font-black leading-[5rem] tracking-tighter">
+          MIT&nbsp;GAME DEVELOPERS GUILD
         </h1>
       </div>
 
-      <div className="h-96 w-96" style={{ opacity: gimOpacity }}>
+      <div
+        className="-mt-28"
+        style={{ opacity: gimOpacity, width: 600, height: 600 }}
+      >
         <Canvas
           camera={{
             position: [20, 10, 20],
-            zoom: 1.5,
+            zoom: 1.8,
             fov: 20,
           }}
         >
