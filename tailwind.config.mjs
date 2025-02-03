66f8c3f14a981af2df00d5fac2281f76777b7a6a
William Y. Feng <womogenes2@gmail.com>
2025-02-02 22:25:40 -0500

    Configure customm shadow

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
+      },
+      boxShadow: {
+        sm: '3px 3px var(--tw-shadow-color, #80808080)',
+        DEFAULT: '10px 10px var(--tw-shadow-color)',
       },
       colors: {
         muted: {
@@ -12,7 +17,12 @@ export default {
           dark: '#d4d4d4',
         },
       },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
