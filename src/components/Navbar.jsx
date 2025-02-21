1edf5e2b3a7af80bbd33c12dd5bb77812f932d08
William Y. Feng <womogenes2@gmail.com>
2025-02-20 23:04:51 -0500

    Add sexy buttons, navbar, game about page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { buttonVariants } from './ui/button';
+
+export default function Navbar() {
+  return (
+    <div className="flex w-full justify-center p-4">
+      <ul className="flex w-full max-w-3xl items-center gap-2">
+        <a className="mr-auto flex items-center gap-1" href="/">
+          <img alt="gim" src="/favicon.ico" width="30" />
+          <div className="font-pixel text-[0.7rem] leading-3">
+            MIT
+            <br />
+            GDG
+          </div>
+        </a>
+        <NavItemLink href="/the-game">The Game</NavItemLink>
+      </ul>
+    </div>
+  );
+}
+
+function NavItemLink({ children, href }) {
+  return (
+    <li>
+      <a
+        className={cn(buttonVariants({ variant: 'ghost' }), 'font-black')}
+        href={href}
+      >
+        {children}
+      </a>
+    </li>
+  );
+}
+
+// className="px-5 py-2 transition-all hover:bg-neutral-800 hover:text-white hover:shadow-sm dark:hover:bg-white dark:hover:text-black"
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,28 +5,28 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 tracking-widest whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-neutral-300',
+  'inline-flex self-start items-center justify-center gap-2 tracking-widest whitespace-nowrap text-sm font-black transition-all focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-neutral-300 tracking-snug',
   {
     variants: {
       variant: {
         default:
-          'bg-neutral-900 text-neutral-50 transition-shadow hover:bg-neutral-900/90 hover:shadow dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90',
+          'bg-neutral-200 text-black shadow-sm hover:bg-neutral-900 hover:text-white hover:shadow-lg dark:bg-neutral-800 dark:text-white dark:hover:bg-neutral-50/90 dark:hover:text-black',
         destructive:
           'bg-red-500 text-neutral-50 transition-shadow hover:bg-red-500/90 hover:shadow-sm dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90',
         primary:
-          'border-none flex self-start gap-3 bg-blue-600 text-white font-black uppercase shadow-sm shadow-blue-400 transition-all hover:shadow-lg hover:bg-blue-500 dark:bg-blue-700 dark:shadow-blue-900 dark:hover:bg-blue-500',
+          'border-none flex bg-blue-600 text-white uppercase shadow-sm shadow-blue-400 transition-all hover:shadow-lg hover:bg-blue-500 dark:bg-blue-700 dark:shadow-blue-900 dark:hover:bg-blue-500',
         outline:
           'border border-neutral-400 shadow-neutral-200 shadow-sm transition-all hover:shadow hover:shadow-md hover:shadow-neutral-400 dark:border-neutral-800 dark:text-neutral-300 dark:shadow-neutral-800 dark:hover:bg-neutral-200 dark:hover:shadow-neutral-500 dark:hover:text-neutral-900 dark:hover:border-neutral-200',
         secondary:
-          'bg-neutral-100 text-neutral-900 transition-shadow hover:bg-neutral-100/80 hover:shadow-sm dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80',
+          'bg-neutral-100 text-neutral-900 transition-shadow hover:bg-neutral-100/80 hover:shadow-md dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80',
         ghost:
-          'hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
+          'hover:shadow-md hover:bg-neutral-900 hover:text-white dark:hover:bg-neutral-200 dark:hover:text-black',
         link: 'text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50',
       },
       size: {
-        default: 'h-9 px-4 py-2',
+        default: 'h-9 px-4 py-2 text-base',
         sm: 'h-8 px-3 text-xs',
-        lg: 'h-10 px-8',
+        lg: 'h-10 px-8 text-base',
         icon: 'h-9 w-9',
       },
     },
